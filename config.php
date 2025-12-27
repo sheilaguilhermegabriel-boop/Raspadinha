f7752f95a019d290a000a40195decda574203ef6
sheilaguilhermegabriel-boop <sheilaguilhermegabriel@gmail.com>
2025-12-27 17:55:12 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,1 +1,15 @@
+<?php
 
+// BANCO DE DADOS
+define('DB_HOST', 'HOST_DO_RAILWAY');
+define('DB_USER', 'USUARIO');
+define('DB_PASS', 'SENHA');
+define('DB_NAME', 'DATABASE');
+
+// GATEWAY
+define('GATEWAY_API_KEY', 'SUA_API_KEY');
+define('GATEWAY_SECRET', 'SEU_SECRET');
+define('GATEWAY_URL', 'https://api.seugateway.com');
+
+// CALLBACK
+define('CALLBACK_URL', 'https://SEUAPP.up.railway.app/api/callback.php');
